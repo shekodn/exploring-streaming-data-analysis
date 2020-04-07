9713bca647b0e092332ef205c5e0ed771eba1d2b
shekodn <sdiazn19@gmail.com>
2020-04-07 09:51:54 -0500

    Setup Lambda's env

--- ch1/deploy.sh
+++ ch1/deploy.sh
@@ -6,6 +6,23 @@ CLI_PROFILE=sdn
 EC2_INSTANCE_TYPE=t2.micro
 KEY_NAME=id_rsa_sergio_2020-03-25
 
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile ${CLI_PROFILE} \
+  --query "Account" --output text`
+
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy --region $REGION \
@@ -16,11 +33,12 @@ aws cloudformation deploy --region $REGION \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE \
-    KeyName=$KEY_NAME
+    KeyName=$KEY_NAME \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile $CLI_PROFILE \
-    --region $REGION | cat  > exports
+    --region $REGION | cat > exports
 fi

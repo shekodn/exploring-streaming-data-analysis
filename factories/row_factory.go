e11909d0506abe498b241e9b86610d34a1795019
shekodn <sdiazn19@gmail.com>
2020-04-10 17:57:18 -0500

    Add factory for SomeRow

--- factories/row_factory.go
+++ factories/row_factory.go
@@ -4,24 +4,17 @@ import (
   "github.com/shekodn/exploring-streaming-data-analysis/models"
 )
 
-// Assamble EVENT based on the type
-func GetRow(event models.Event_iface) models.Row_iface {
+func GetSomeRow(event models.Event_iface) models.Row_iface {
 	switch event.(type)  {
 
     case *models.TruckArrives:
-      return models.SomeRow{}
+      return &models.SomeRow{}
 
     case *models.TruckDeparts:
-      return models.SomeRow{}
+      return &models.SomeRow{}
 
     case *models.MechanicChangesOil:
-      return models.NoneRow{}
-
-    case *models.DriverDeliversPackage:
-      return models.NoneRow{}
-
-    case *models.DriverMissesCustomer:
-      return models.NoneRow{}
+      return &models.SomeRow{}
 
     default:
   		return nil

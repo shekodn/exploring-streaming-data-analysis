ca1cc37fc48b59db40d45146a479f828d59e1c94
shekodn <sdiazn19@gmail.com>
2020-04-10 14:34:11 -0500

    Add factory methods

--- models/event.go
+++ models/event.go
@@ -10,100 +10,116 @@ import (
 //// Type of events
 // Creates event interface in order to be able to create an event according
 // to the type
-type Event_iface interface {}
+type Event_iface interface {
+    Assemble(dataBytes []byte)
+    String() string
+}
+
+type Event struct {
+  Timestamp time.Time
+}
 
 type EventSniffer struct {
   Event string
 }
 
 type TruckArrives struct {
-  Timestamp time.Time
+  Event
   Vehicle Vehicle
   Location Location
 }
 
 type TruckDeparts struct {
-  Timestamp time.Time
+  Event
   Vehicle Vehicle
   Location Location
 }
 
 type MechanicChangesOil struct {
-  Timestamp time.Time
+  Event
   Employee Employee
   Vehicle Vehicle
 }
 
 type DriverDeliversPackage struct {
-  Timestamp time.Time
+  Event
   Employee Employee
   // `package`: Package, customer: Customer, location: Location)
 }
 
 type DriverMissesCustomer struct {
-  Timestamp time.Time
+  Event
   Employee Employee
   // `package`: Package, customer: Customer, location: Location)
 }
 
-// Factory - Assamble
-func GetTypeOfEvent(event string, req []byte) Event_iface {
-	switch event {
+// Interfcae Methods
 
-    case "TRUCK_ARRIVES":
-      return NewTruckArrives(req)
-
-    case "TRUCK_DEPARTS":
-      return NewTruckDeparts(req)
-
-    case "MECHANIC_CHANGES_OIL":
-      return NewMechanicChangesOil(req)
-
-    case "DRIVER_DELIVERS_PACKAGE":
-      return NewDriverDeliversPackage(req)
-
-    case "DRIVER_MISSES_CUSTOMER":
-      return NewDriverMissesCustomer(req)
-
-    default:
-  		fmt.Println("type undefined")
-  		return nil
-  }
-}
-
-
-// Factory Methods
-func NewTruckArrives(req []byte) *TruckArrives {
-	event := &TruckArrives{}
-  json.Unmarshal(req, &event)
-	var _ Event_iface = event // Enforce interface compliance
-	return event
-}
-
-func NewTruckDeparts(req []byte) *TruckDeparts {
-  event := &TruckDeparts{}
-  json.Unmarshal(req, &event)
+func (event *TruckArrives) Assemble(dataBytes []byte) {
+	fmt.Println("Assembling components for TruckArrives")
+  json.Unmarshal(dataBytes, &event)
   var _ Event_iface = event // Enforce interface compliance
-  return event
 }
 
-func NewMechanicChangesOil(req []byte) *MechanicChangesOil {
-  event := &MechanicChangesOil{}
-  json.Unmarshal(req, &event)
+func (event *TruckDeparts) Assemble(dataBytes []byte) {
+	fmt.Println("Assembling components for TruckDeparts")
+  json.Unmarshal(dataBytes, &event)
   var _ Event_iface = event // Enforce interface compliance
-  return event
 }
 
-func NewDriverDeliversPackage(req []byte) *DriverDeliversPackage {
-  event := &DriverDeliversPackage{}
-  json.Unmarshal(req, &event)
+func (event *MechanicChangesOil) Assemble(dataBytes []byte) {
+	fmt.Println("Assembling components for MechanicChangesOil")
+  json.Unmarshal(dataBytes, &event)
   var _ Event_iface = event // Enforce interface compliance
-  return event
 }
 
-func NewDriverMissesCustomer(req []byte) *DriverMissesCustomer {
-  event := &DriverMissesCustomer{}
-  json.Unmarshal(req, &event)
+func (event *DriverDeliversPackage) Assemble(dataBytes []byte) {
+	fmt.Println("Assembling components for DriverDeliversPackage")
+  json.Unmarshal(dataBytes, &event)
   var _ Event_iface = event // Enforce interface compliance
-  return event
+}
+
+func (event *DriverMissesCustomer) Assemble(dataBytes []byte) {
+	fmt.Println("Assembling components for DriverMissesCustomer")
+  json.Unmarshal(dataBytes, &event)
+  var _ Event_iface = event // Enforce interface compliance
+}
+
+
+func (e TruckArrives) String() string {
+    return fmt.Sprintf("TruckArrives: %s - %s - %s - %s - %s - %s",
+      e.Timestamp,
+      e.Vehicle.Vin, e.Vehicle.Mileage,
+      e.Location.Latitude, e.Location.Longitude, e.Location.Elevation,
+    )
+}
+
+func (e TruckDeparts) String() string {
+    return fmt.Sprintf("TruckDeparts: %s - %s - %s - %s - %s - %s",
+      e.Timestamp,
+      e.Vehicle.Vin, e.Vehicle.Mileage,
+      e.Location.Latitude, e.Location.Longitude, e.Location.Elevation,
+    )
+}
+
+func (e MechanicChangesOil) String() string {
+    return fmt.Sprintf("MechanicChangesOil: %s - %s - %s - %s - %s",
+      e.Timestamp,
+      e.Employee.Id, e.Employee.JobRole,
+      e.Vehicle.Vin, e.Vehicle.Mileage,
+    )
+}
+
+func (e DriverDeliversPackage) String() string {
+    return fmt.Sprintf("DriverDeliversPackage: %s - %s - %s",
+      e.Timestamp,
+      e.Employee.Id, e.Employee.JobRole,
+    )
+}
+
+func (e DriverMissesCustomer) String() string {
+    return fmt.Sprintf("DriverMissesCustomer: %s - %s - %s",
+      e.Timestamp,
+      e.Employee.Id, e.Employee.JobRole,
+    )
 }
